1df323be385ffdadaf72038ac008607cb93ca487
Eric Villarreal <35571755+sclickk@users.noreply.github.com>
2020-09-13 23:05:47 -0500

    Use ES6 classes
    I used the VS Code's "source action" feature to refactor this

--- js/classlist_polyfill.js
+++ js/classlist_polyfill.js
@@ -9,40 +9,45 @@
       splice = prototype.splice,
       join = prototype.join;
 
-  function DOMTokenList(el) {
-    this.el = el;
-    // The className needs to be trimmed and split on whitespace
-    // to retrieve a list of classes.
-    var classes = el.className.replace(/^\s+|\s+$/g, '').split(/\s+/);
-    for (var i = 0; i < classes.length; i++) {
-      push.call(this, classes[i]);
+  class DOMTokenList {
+    constructor(el) {
+      this.el = el;
+      // The className needs to be trimmed and split on whitespace
+      // to retrieve a list of classes.
+      var classes = el.className.replace(/^\s+|\s+$/g, '').split(/\s+/);
+      for (var i = 0; i < classes.length; i++) {
+        push.call(this, classes[i]);
+      }
     }
-  }
 
-  DOMTokenList.prototype = {
-    add: function (token) {
+    add(token) {
       if (this.contains(token)) return;
       push.call(this, token);
       this.el.className = this.toString();
-    },
-    contains: function (token) {
+    }
+
+    contains(token) {
       return this.el.className.indexOf(token) != -1;
-    },
-    item: function (index) {
+    }
+
+    item(index) {
       return this[index] || null;
-    },
-    remove: function (token) {
+    }
+
+    remove(token) {
       if (!this.contains(token)) return;
       for (var i = 0; i < this.length; i++) {
         if (this[i] == token) break;
       }
       splice.call(this, i, 1);
       this.el.className = this.toString();
-    },
-    toString: function () {
+    }
+
+    toString() {
       return join.call(this, ' ');
-    },
-    toggle: function (token) {
+    }
+
+    toggle(token) {
       if (!this.contains(token)) {
         this.add(token);
       } else {
@@ -51,7 +56,8 @@
 
       return this.contains(token);
     }
-  };
+  }
+
 
   window.DOMTokenList = DOMTokenList;
 
